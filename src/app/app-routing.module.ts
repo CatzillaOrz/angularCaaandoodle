032b14eaa29adec43bf0387a7be8abfce4816e6a
catzillaorz <catzillaorz@outlook.com>
2019-03-15 15:31:42 +0800

    Milestone | Lazy load the crisis center

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,32 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RouterModule, Routes } from '@angular/router';
+
 import { ComposeMessageComponent } from './compose-message/compose-message.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 import { AuthGuard } from './auth/auth.guard';
 
 const appRoutes: Routes = [
-  /* . . . */
-
+  {
+    path: 'compose',
+    component: ComposeMessageComponent,
+    outlet: 'popup'
+  },
   {
     path: 'admin',
     loadChildren: './admin/admin.module#AdminModule',
     canLoad: [AuthGuard]
   },
   {
-    path: 'compose',
-    component: ComposeMessageComponent,
-    outlet: 'popup'
+    path: 'crisis-center',
+    loadChildren: './crisis-center/crisis-center.module#CrisisCenterModule'
   },
   { path: '', redirectTo: '/heroes', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes, { enableTracing: true })],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { Router } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ComposeMessageComponent } from './compose-message/compose-message.component';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { CrisisCenterModule } from './crisis-center/crisis-center.module';
-import { ComposeMessageComponent } from './compose-message/compose-message.component';
 import { AuthModule } from './auth/auth.module';
-// import { CrisisModule } from './crisis-center/crisis.module'; //<-- never used
-
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PageNotFoundComponent,
-    ComposeMessageComponent
-  ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HeroesModule,
-    BrowserAnimationsModule,
-    CrisisCenterModule,
     AuthModule,
-    // CrisisModule,
-    AppRoutingModule,
+    AppRoutingModule
   ],
-  providers: [],
+  declarations: [AppComponent, ComposeMessageComponent, PageNotFoundComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
