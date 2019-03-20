0ebb848b21ea2a475dfa65e2785a98a3d1ec81ae
catzillaorz <catzillaorz@outlook.com>
2019-03-20 18:24:15 +0800

    Grouping form controls

--- src/app/form-cook/form-cook.module.ts
+++ src/app/form-cook/form-cook.module.ts
@@ -5,9 +5,10 @@ import { FormCookRoutingModule } from './form-cook-routing.module';
 import { IndexComponent } from './index/index.component';
 import { NameEditorComponent } from './name-editor/name-editor.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
 
 @NgModule({
-  declarations: [IndexComponent, NameEditorComponent],
+  declarations: [IndexComponent, NameEditorComponent, ProfileEditorComponent],
   imports: [CommonModule, FormCookRoutingModule, ReactiveFormsModule],
 })
 export class FormCookModule {}
